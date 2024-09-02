a601d602f438255e8a2a4c1944ad8a1f24fd308b
masa-stefanovic <masa.v.stefanovic>
2024-09-02 18:39:39 +0200

    dodate ikone u GUI za glavni editor

--- client.py
+++ client.py
@@ -7,82 +7,113 @@ class TextEditor:
     def __init__(self, root):
         self.root = root
         self.root.title("Collaborative Word")
-        self.root.geometry("1920x1080")
+        self.root.geometry("1980x1080")
 
+        # Initialize font variables
+        self.font_var = tk.StringVar(value="Arial")
+        self.font_size_var = tk.StringVar(value="12")
+        
+        # Initialize file variable
+        self.current_file = None
+
+        # Toolbar and text area setup
+        self.toolbar = tk.Frame(self.root, bd=1, relief=tk.RAISED)
+        self.toolbar.pack(side=tk.TOP, fill=tk.X)
         self.text_area = tk.Text(self.root, wrap='word', undo=True)
         self.text_area.pack(fill='both', expand=True)
 
-        self.main_menu = tk.Menu()
-        self.root.config(menu=self.main_menu)
+        # Create toolbar icons and functionality
+        self.add_toolbar_icons()
+
+    def add_toolbar_icons(self):
+        """Adds icons to the toolbar for various functionalities."""
+        icon_size = (24, 24)
         
-        # File menu
-        file_menu = tk.Menu(self.main_menu, tearoff=False)
-        self.main_menu.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="Save", command=self.save_file)
-        file_menu.add_command(label="Save As", command=self.save_as_file)
-        file_menu.add_command(label="Insert Image", command=self.insert_image)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.root.quit)
+        # Icons for toolbar buttons
+        icons = {
+            "file": Image.open("static/save-icon.png").resize(icon_size, Image.LANCZOS),
+            "clipboard": Image.open("static/clipboard-icon.png").resize(icon_size, Image.LANCZOS),
+            "bold": Image.open("static/bold-icon.png").resize(icon_size, Image.LANCZOS),
+            "italic": Image.open("static/itallic-icon.png").resize(icon_size, Image.LANCZOS),
+            "underline": Image.open("static/underline-icon.png").resize(icon_size, Image.LANCZOS),
+            "text_color": Image.open("static/textcolor-icon.png").resize(icon_size, Image.LANCZOS),
+            "insert_image": Image.open("static/image-icon.png").resize(icon_size, Image.LANCZOS),
+            "insert_link": Image.open("static/link-icon.png").resize(icon_size, Image.LANCZOS),
+            "change_font": Image.open("static/font-icon.png").resize(icon_size, Image.LANCZOS)
+        }
 
-        # Clipboard menu
-        edit_menu = tk.Menu(self.main_menu, tearoff=False)
-        self.main_menu.add_cascade(label="Clipboard", menu=edit_menu)
-        edit_menu.add_command(label="Copy", command=self.copy_text)
-        edit_menu.add_command(label="Cut", command=self.cut_text)
-        edit_menu.add_separator()
-        edit_menu.add_command(label="Paste", command=self.paste_text)
-        
-        # Edit menu
-        edit_menu = tk.Menu(self.main_menu, tearoff=False)
-        self.main_menu.add_cascade(label="Edit", menu=edit_menu)
-        edit_menu.add_command(label="Undo", command=self.text_area.edit_undo)
-        edit_menu.add_command(label="Redo", command=self.text_area.edit_redo)
+        # Convert images to PhotoImage objects
+        self.icons = {name: ImageTk.PhotoImage(icon) for name, icon in icons.items()}
 
-        # Format menu
-        format_menu = tk.Menu(self.main_menu, tearoff=False)
-        self.main_menu.add_cascade(label="Format", menu=format_menu)
-        format_menu.add_command(label="Bold", command=self.make_bold)
-        format_menu.add_command(label="Italic", command=self.make_italic)
-        format_menu.add_command(label="Underline", command=self.make_underline)
-        format_menu.add_separator()
-        format_menu.add_command(label="Insert Hyperlink", command=self.insert_hyperlink_with_hover)
-        format_menu.add_command(label="Text Color", command=self.change_text_color)
+        # File Button with Dropdown Menu
+        file_button = tk.Menubutton(self.toolbar, image=self.icons["file"], relief=tk.RAISED)
+        file_button.pack(side=tk.LEFT, padx=2, pady=2)
+        file_button.menu = tk.Menu(file_button, tearoff=0)
+        file_button["menu"] = file_button.menu
+        self.create_file_menu(file_button.menu)
 
-        # Font menu
-        font_menu = tk.Menu(self.main_menu, tearoff=False)
-        self.main_menu.add_cascade(label="Font", menu=font_menu)
+        # Clipboard Button with Dropdown Menu
+        clipboard_button = tk.Menubutton(self.toolbar, image=self.icons["clipboard"], relief=tk.RAISED)
+        clipboard_button.pack(side=tk.LEFT, padx=2, pady=2)
+        clipboard_button.menu = tk.Menu(clipboard_button, tearoff=0)
+        clipboard_button["menu"] = clipboard_button.menu
+        self.create_clipboard_menu(clipboard_button.menu)
 
-        # Font Family
-        self.font_var = tk.StringVar(value="Arial")
+        # Font Button with Dropdown Menu
+        font_button = tk.Menubutton(self.toolbar, image=self.icons["change_font"], relief=tk.RAISED)
+        font_button.pack(side=tk.LEFT, padx=2, pady=2)
+        font_button.menu = tk.Menu(font_button, tearoff=0)
+        font_button["menu"] = font_button.menu
+        self.create_font_menu(font_button.menu)
+
+        # Other Toolbar Buttons
+        tk.Button(self.toolbar, image=self.icons["bold"], command=self.make_bold).pack(side=tk.LEFT, padx=2, pady=2)
+        tk.Button(self.toolbar, image=self.icons["italic"], command=self.make_italic).pack(side=tk.LEFT, padx=2, pady=2)
+        tk.Button(self.toolbar, image=self.icons["underline"], command=self.make_underline).pack(side=tk.LEFT, padx=2, pady=2)
+        tk.Button(self.toolbar, image=self.icons["text_color"], command=self.change_text_color).pack(side=tk.LEFT, padx=2, pady=2)
+        tk.Button(self.toolbar, image=self.icons["insert_image"], command=self.insert_image).pack(side=tk.LEFT, padx=2, pady=2)
+        tk.Button(self.toolbar, image=self.icons["insert_link"], command=self.insert_link).pack(side=tk.LEFT, padx=2, pady=2)
+
+    def create_file_menu(self, menu):
+        """Creates a dropdown menu for file operations."""
+        menu.add_command(label="New", command=self.new_file)
+        menu.add_command(label="Open", command=self.open_file)
+        menu.add_command(label="Save", command=self.save_file)
+        menu.add_command(label="Save As", command=self.save_as_file)
+
+    def create_clipboard_menu(self, menu):
+        """Creates a dropdown menu for clipboard operations."""
+        menu.add_command(label="Copy", command=self.copy_text)
+        menu.add_command(label="Cut", command=self.cut_text)
+        menu.add_command(label="Paste", command=self.paste_text)
+
+    def create_font_menu(self, menu):
+        """Creates a dropdown menu for selecting font family and size."""
         font_families = sorted(font.families())
-        font_menu.add_cascade(label="Font Family", menu=self.create_font_family_menu(font_families))
 
-        # Font Size
-        self.font_size_var = tk.StringVar(value="12")
-        font_menu.add_cascade(label="Font Size", menu=self.create_font_size_menu())
-
-        self.current_file = None
-
-    def create_font_family_menu(self, font_families):
-        font_family_menu = tk.Menu(self.main_menu, tearoff=False)
+        # Create submenu for font families
+        font_family_menu = tk.Menu(menu, tearoff=False)
         for family in font_families:
             font_family_menu.add_radiobutton(label=family, variable=self.font_var, command=self.apply_font)
-        return font_family_menu
-
-    def create_font_size_menu(self):
-        font_size_menu = tk.Menu(self.main_menu, tearoff=False)
+        
+        # Create submenu for font sizes
+        font_size_menu = tk.Menu(menu, tearoff=False)
         for size in range(8, 72, 2):
             font_size_menu.add_radiobutton(label=str(size), variable=self.font_size_var, command=self.apply_font)
-        return font_size_menu
+        
+        menu.add_cascade(label="Font Family", menu=font_family_menu)
+        menu.add_cascade(label="Font Size", menu=font_size_menu)
 
     def apply_font(self):
         selected_font = font.Font(family=self.font_var.get(), size=self.font_size_var.get())
         tag_name = f"font_{self.font_var.get()}_{self.font_size_var.get()}"
+
         self.text_area.tag_configure(tag_name, font=selected_font)
+
         try:
             self.text_area.tag_add(tag_name, "sel.first", "sel.last")
         except tk.TclError:
-            pass
+            pass  # Ignore if no text is selected
 
     def change_text_color(self):
         color = colorchooser.askcolor()[1]
@@ -119,184 +150,76 @@ class TextEditor:
 
     def open_file(self):
         self.current_file = filedialog.askopenfilename(defaultextension=".txt",
-                                                      filetypes=[("Text Files", "*.txt"),
-                                                                 ("All Files", "*.*")])
+                                                       filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
         if self.current_file:
             with open(self.current_file, "r") as file:
+                content = file.read()
                 self.text_area.delete(1.0, tk.END)
-                self.text_area.insert(1.0, file.read())
+                self.text_area.insert(tk.END, content)
 
     def save_file(self):
-        if self.current_file:
-            content = self.text_area.get(1.0, tk.END)
-            with open(self.current_file, "w") as file:
-                file.write(content)
-        else:
+        if not self.current_file:
             self.save_as_file()
+        else:
+            with open(self.current_file, "w") as file:
+                file.write(self.text_area.get(1.0, tk.END))
 
     def save_as_file(self):
         self.current_file = filedialog.asksaveasfilename(defaultextension=".txt",
-                                                         filetypes=[("Text Files", "*.txt"),
-                                                                    ("All Files", "*.*")])
+                                                         filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
         if self.current_file:
             with open(self.current_file, "w") as file:
                 file.write(self.text_area.get(1.0, tk.END))
 
     def make_bold(self):
         current_tags = self.text_area.tag_names("sel.first")
-        if "italic" in current_tags and "underline" in current_tags:
-            self.make_bolditalicunderline()
-        elif "italic" in current_tags:
-            self.make_bolditalic()
-        elif "underline" in current_tags:
-            self.make_boldunderline()
+        if "bold" in current_tags:
+            self.text_area.tag_remove("bold", "sel.first", "sel.last")
         else:
             bold_font = font.Font(self.text_area, self.text_area.cget("font"))
             bold_font.configure(weight="bold")
             self.text_area.tag_configure("bold", font=bold_font)
-
-            if "bold" in current_tags:
-                self.text_area.tag_remove("bold", "sel.first", "sel.last")
-            else:
-                self.text_area.tag_add("bold", "sel.first", "sel.last")
+            self.text_area.tag_add("bold", "sel.first", "sel.last")
 
     def make_italic(self):
         current_tags = self.text_area.tag_names("sel.first")
-        if "bold" in current_tags and "underline" in current_tags:
-            self.make_bolditalicunderline()
-        elif "bold" in current_tags:
-            self.make_bolditalic()
-        elif "underline" in current_tags:
-            self.make_italicunderline()
+        if "italic" in current_tags:
+            self.text_area.tag_remove("italic", "sel.first", "sel.last")
         else:
             italic_font = font.Font(self.text_area, self.text_area.cget("font"))
             italic_font.configure(slant="italic")
             self.text_area.tag_configure("italic", font=italic_font)
-
-            if "italic" in current_tags:
-                self.text_area.tag_remove("italic", "sel.first", "sel.last")
-            else:
-                self.text_area.tag_add("italic", "sel.first", "sel.last")
+            self.text_area.tag_add("italic", "sel.first", "sel.last")
 
     def make_underline(self):
         current_tags = self.text_area.tag_names("sel.first")
-        if "bolditalic" in current_tags:
-            self.make_bolditalicunderline()
-        elif "bold" in current_tags:
-            self.make_boldunderline()
-        elif "italic" in current_tags:
-            self.make_italicunderline()
+        if "underline" in current_tags:
+            self.text_area.tag_remove("underline", "sel.first", "sel.last")
         else:
             underline_font = font.Font(self.text_area, self.text_area.cget("font"))
-            underline_font.configure(underline=True)
+            underline_font.configure(underline=1)
             self.text_area.tag_configure("underline", font=underline_font)
-
-            if "underline" in current_tags:
-                self.text_area.tag_remove("underline", "sel.first", "sel.last")
-            else:
-                self.text_area.tag_add("underline", "sel.first", "sel.last")
-
-    def make_bolditalic(self):
-        bolditalic_font = font.Font(self.text_area, self.text_area.cget("font"))
-        bolditalic_font.configure(weight="bold", slant="italic")
-        self.text_area.tag_configure("bolditalic", font=bolditalic_font)
-
-        current_tags = self.text_area.tag_names("sel.first")
-        if "bolditalic" in current_tags:
-            self.text_area.tag_remove("bolditalic", "sel.first", "sel.last")
-        else:
-            self.text_area.tag_add("bolditalic", "sel.first", "sel.last")
-
-        if "bold" in current_tags:
-            self.text_area.tag_remove("bold", "sel.first", "sel.last")
-        if "italic" in current_tags:
-            self.text_area.tag_remove("italic", "sel.first", "sel.last")
-
-    def make_boldunderline(self):
-        boldunderline_font = font.Font(self.text_area, self.text_area.cget("font"))
-        boldunderline_font.configure(weight="bold", underline=True)
-        self.text_area.tag_configure("boldunderline", font=boldunderline_font)
-
-        current_tags = self.text_area.tag_names("sel.first")
-        if "boldunderline" in current_tags:
-            self.text_area.tag_remove("boldunderline", "sel.first", "sel.last")
-        else:
-            self.text_area.tag_add("boldunderline", "sel.first", "sel.last")
-
-        if "bold" in current_tags:
-            self.text_area.tag_remove("bold", "sel.first", "sel.last")
-        if "underline" in current_tags:
-            self.text_area.tag_remove("underline", "sel.first", "sel.last")
-
-    def make_italicunderline(self):
-        italicunderline_font = font.Font(self.text_area, self.text_area.cget("font"))
-        italicunderline_font.configure(slant="italic", underline=True)
-        self.text_area.tag_configure("italicunderline", font=italicunderline_font)
-
-        current_tags = self.text_area.tag_names("sel.first")
-        if "italicunderline" in current_tags:
-            self.text_area.tag_remove("italicunderline", "sel.first", "sel.last")
-        else:
-            self.text_area.tag_add("italicunderline", "sel.first", "sel.last")
-
-        if "italic" in current_tags:
-            self.text_area.tag_remove("italic", "sel.first", "sel.last")
-        if "underline" in current_tags:
-            self.text_area.tag_remove("underline", "sel.first", "sel.last")
-
-    def make_bolditalicunderline(self):
-        bolditalicunderline_font = font.Font(self.text_area, self.text_area.cget("font"))
-        bolditalicunderline_font.configure(weight="bold", slant="italic", underline=True)
-        self.text_area.tag_configure("bolditalicunderline", font=bolditalicunderline_font)
-        current_tags = self.text_area.tag_names("sel.first")
-        if "bolditalicunderline" in current_tags:
-            self.text_area.tag_remove("bolditalicunderline", "sel.first", "sel.last")
-        else:
-            self.text_area.tag_add("bolditalicunderline", "sel.first", "sel.last")
-
-        if "bold" in current_tags:
-            self.text_area.tag_remove("bold", "sel.first", "sel.last")
-        if "italic" in current_tags:
-            self.text_area.tag_remove("italic", "sel.first", "sel.last")
-        if "underline" in current_tags:
-            self.text_area.tag_remove("underline", "sel.first", "sel.last")
-
-    def insert_hyperlink_with_hover(self):
-        url = simpledialog.askstring("Insert Hyperlink", "Enter URL:")
-        if url:
-            display_text = simpledialog.askstring("Insert Hyperlink", "Enter the display text:")
-            if not display_text:
-                display_text = url  
-
-        link = self.hyperlink(self.root, url, text=display_text)
-        self.text_area.window_create(tk.INSERT, window=link)
-
-
-    def hyperlink(self, root, link: str, **kwargs) -> tk.Label:
-        
-        for i in (("fg", "magenta"), ("text", "Hyperlink!"), ("font", "None 10"), ("cursor", "hand2")):
-            kwargs.setdefault(i[0], i[1])
-
-        
-        label = tk.Label(root, **kwargs)
-        label.link = link
-
-        
-        label.bind("<Button-1>", lambda e: webbrowser.open(e.widget.link))
-        label.bind("<Enter>", lambda e: e.widget.configure(font=e.widget.cget("font") + " underline"))
-        label.bind("<Leave>", lambda e: e.widget.configure(font=e.widget.cget("font")[:-10]))
-
-        return label
+            self.text_area.tag_add("underline", "sel.first", "sel.last")
 
     def insert_image(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
         if file_path:
             image = Image.open(file_path)
-            image.thumbnail((300, 300))
-            self.image = ImageTk.PhotoImage(image)
-            self.text_area.image_create(tk.INSERT, image=self.image)
+            image.thumbnail((100, 100))
+            photo = ImageTk.PhotoImage(image)
+            self.text_area.image_create(tk.END, image=photo)
+            self.text_area.image_list.append(photo)  # Save reference
+
+    def insert_link(self):
+        """Insert a hyperlink into the text area."""
+        url = simpledialog.askstring("Insert Link", "Enter URL:")
+        if url:
+            self.text_area.insert(tk.INSERT, url)
+            self.text_area.tag_add("hyperlink", tk.INSERT + "-%dc" % len(url), tk.INSERT)
+            self.text_area.tag_config("hyperlink", foreground="blue", underline=True)
+            self.text_area.tag_bind("hyperlink", "<Button-1>", lambda e: webbrowser.open(url))
 
 if __name__ == "__main__":
     root = tk.Tk()
-    editor = TextEditor(root)
+    app = TextEditor(root)
     root.mainloop()
